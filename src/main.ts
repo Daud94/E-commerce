2c71a186eec83da11d53d13cd057d8247bdcb25e
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2024-08-30 10:15:13 +0100

    Project setup and configurations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,46 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AllExceptionsFilter } from './middlewares/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(helmet());
+  app.use(helmet.xssFilter());
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:4000',
+      'http://localhost:5173',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Date', 'Content-Type', 'Origin', 'Authorization'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix(process.env.API_VERSION);
+
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce API Documentation')
+    .setDescription('E-commerce API Documentation')
+    .setVersion('v1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  const adapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(adapterHost));
+  const PORT = parseInt(process.env['PORT']) || 6000;
+  await app.listen(PORT);
 }
 bootstrap();
---
+++ src/middlewares/all-exceptions.filter.ts
@@ -0,0 +1,36 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: any, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+    console.error(exception);
+    const ctx = host.switchToHttp();
+    let httpStatus: number;
+    if (exception instanceof HttpException) {
+      httpStatus = exception.getStatus() || HttpStatus.INTERNAL_SERVER_ERROR;
+    } else if (exception instanceof TypeError) {
+      httpStatus = HttpStatus.BAD_REQUEST;
+    } else {
+      httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+    const responseBody = {
+      success: false,
+      message:
+        exception.response.message ??
+        exception.response ??
+        'Something went wrong',
+    };
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
